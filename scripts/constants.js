3be99c02494c7683e79db8332eb9515d172bd985
mpm11011 <mark.mauriello@uconn.edu>
2021-12-21 20:11:05 -0500

    Add constants.

---
+++ scripts/constants.js
@@ -0,0 +1,38 @@
+var imagePath = "./worlds/images/";
+var jsonPath = "./worlds/json/";
+var splitter = "_";
+
+var hilightTwoWay = {
+	'strokeColor': '0080ff',
+	'strokeWidth': 4,
+	'fillColor': '0080ff',
+	'fillOpacity' :0.4
+};
+
+var hilightOneWay = {
+	'strokeColor': '00ffff',
+	'strokeWidth': 4,
+	'fillColor': '00ffff',
+	'fillOpacity': 0.4
+};
+
+var hilightUnlinked = {
+	'strokeColor': 'ffffff',
+	'strokeWidth': 4,
+	'fillColor': 'ffffff',
+	'fillOpacity': 0.4
+};
+
+var hilightDeadEnd = {
+	'strokeColor': '000000',
+	'strokeWidth': 4,
+	'fillColor': '000000',
+	'fillOpacity': 0.4
+};
+
+var hilightKeyLocation = {
+	'strokeColor': 'ffff00',
+	'strokeWidth': 4,
+	'fillColor': 'ffff00',
+	'fillOpacity': 0.4
+};
